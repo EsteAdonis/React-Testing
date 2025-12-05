3cc5130c855cb4e6999a30c434035c8e52bebe2d
EsteAdonis <julio.e.ochoa.p@gmail.com>
2025-12-04 18:39:19 -0600

    Wrapper options, custom render, Test Cutom React Hooks and Act utility

--- src/hooks/use-counter/useCounter.test.tsx
+++ src/hooks/use-counter/useCounter.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react';
 import { useCounter } from './useCounter';
 
 describe ('useCounter', () => {
@@ -10,5 +10,17 @@ describe ('useCounter', () => {
 	it('should accept and render the same inital count', () => {
 		const { result } = renderHook(useCounter, { initialProps: { initialCount: 10},})
 		expect(result.current.count).toBe(10);
+	})
+
+	it('should increment the count', () => {
+		const { result } = renderHook(useCounter);
+		act(() => result.current.increment());
+		expect(result.current.count).toBe(1);
+	})
+
+	it('should decrement the count', () => {
+		const { result } = renderHook(useCounter, { initialProps: { initialCount: 5},})
+		act(() => result.current.decrement());
+		expect(result.current.count).toBe(4);
 	})
 })
